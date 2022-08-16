02e732f167fd3a4b202d9984bee86d4fa5d492dc
Mutuma Gitonga <mgitonga150@gmail.com>
2022-08-16 21:26:33 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(funActivity = "roller-skate") {
+  return `This Saturday, I want to ${funActivity}!`;
+}
+
+function mondayWork(task = "go to the office") {
+  return `This Monday, I will ${task}.`
+}
+
+function wrapAdjective(flair = "*") {
+  return function(matter = "special") {
+    return `You are ${flair}${matter}${flair}!`;
+  }
+}
